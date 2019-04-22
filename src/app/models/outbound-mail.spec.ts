8b2320c9b38f8789836f2d17e88657accb1e1e37
Tim Boeckmann <tim@bespecific.co.uk>
2019-04-22 21:48:17 +0100

    add some models

---
+++ src/app/models/outbound-mail.spec.ts
@@ -0,0 +1,7 @@
+import { OutboundMail } from './outbound-mail';
+
+describe('OutboundMail', () => {
+  it('should create an instance', () => {
+    expect(new OutboundMail()).toBeTruthy();
+  });
+});
