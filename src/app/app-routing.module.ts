90a9a9a9a4442aeed871e25b8bd2742f905c47c4
Tim Boeckmann <tim@bespecific.co.uk>
2019-04-22 20:46:53 +0100

    add inbox

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InboxComponent } from './inbox/inbox.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 const routes: Routes = [
   {
     path: '',
+    component: InboxComponent,
   }
 ];
 
 @NgModule({
   declarations: [
+    InboxComponent,
   ],
   imports: [
     FormsModule,
---
+++ src/app/inbox/inbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inbox',
+  templateUrl: './inbox.component.html',
+  styleUrls: ['./inbox.component.sass']
+})
+export class InboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
