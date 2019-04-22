f4fc486db204e44bcc1a91caa8d0dbfc67308e36
Tim Boeckmann <tim@bespecific.co.uk>
2019-04-22 20:51:42 +0100

    add HttpHelper provider

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { InboxComponent } from './inbox/inbox.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpHelpersService } from './services/helpers/http-helpers/http-helpers.service';
 
 const routes: Routes = [
   {
@@ -20,6 +21,7 @@ const routes: Routes = [
     CommonModule,
     RouterModule.forRoot(routes)
   ],
+  providers: [HttpHelpersService],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
