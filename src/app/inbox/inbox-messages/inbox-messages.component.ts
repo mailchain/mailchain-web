6ca455cee9c43ac74951052f2b860fb24c824583
Tim Boeckmann <tim@bespecific.co.uk>
2019-04-24 18:20:12 +0100

    complete inbox-messages view

---
+++ src/app/inbox/inbox-messages/inbox-messages.component.ts
@@ -0,0 +1,172 @@
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
+import { MailchainService } from 'src/app/services/mailchain/mailchain.service';
+import { ReadService } from 'src/app/services/mailchain/messages/read.service';
+import { InboundMail } from 'src/app/models/inbound-mail';
+
+@Component({
+  selector: '[inbox-messages]',
+  templateUrl: './inbox-messages.component.html',
+  styleUrls: ['./inbox-messages.component.scss']
+})
+export class InboxMessagesComponent implements OnInit, OnChanges {
+  @Input() messagesLoaded: boolean;
+  @Input() currentAccount: string;
+  @Input() inboxMessages: Array<any>;
+  
+  @Output() openMessage = new EventEmitter();
+  @Output() inboxCounter = new EventEmitter<any>();
+  
+  
+  public currentAccountInboxMessages: Array<any> = [];
+  searchText: string = '';
+
+  constructor(
+    private mailchainService: MailchainService,
+    private readService: ReadService,
+
+  ) {
+  }
+
+  emitInboxCount(addresses: Array<any>){
+    
+    addresses.forEach(address => {
+      // emit message count for inbox component
+      var inboxCount = 0
+      var _this = this
+      this.mailchainService.dedupeMessagesByIds(this.inboxMessages).map(function(msg) {        
+        var addr = _this.parseAddressFromMailchain(msg["headers"]["to"]).toLowerCase()
+        
+        if (addr.includes(address.toLowerCase()) && !msg.read) {          
+          ++inboxCount
+        }
+      });
+      
+      this.inboxCounter.emit([address,inboxCount]);  
+    });
+    
+  }
+
+  /**
+   * 
+   * @param decryptedMsg 
+   */
+  addMailToInboxMessages(decryptedMsg){  
+    decryptedMsg.senderIdenticon = this.generateIdenticon(decryptedMsg.headers.from)
+    var msg = {
+      ...new InboundMail,
+      ...decryptedMsg
+    }
+    this.inboxMessages.push(msg)
+  }
+
+  openMail(mail: any): void {
+    this.readService.markRead(mail["headers"]["message-id"]).subscribe(res => {
+      mail.read = true;
+      this.openMessage.emit(mail);
+      this.emitInboxCount([this.parseAddressFromMailchain(mail["headers"]["to"])])
+    })
+  }
+
+  private generateIdenticon(address){
+    return this.mailchainService.generateIdenticon(address)
+  }
+  
+  private parseAddressFromMailchain(address){
+    return this.mailchainService.parseAddressFromMailchain(address)
+  }
+
+  /**
+   * Toggles a message `selected` attribute 
+   * @param message a message 
+   */
+  public selectMail(message: any): void {
+    message.selected = message.selected ? false : true;
+  }
+
+  /**
+   * Selects all messages in the current selected address
+   */
+  public selectAll(): void {
+    for (var mail of this.currentAccountInboxMessages) {
+      mail.selected = true;
+    }
+  }
+  
+  /**
+   * Selects no messages
+   * NOTE: This de-selects all inbox messages, not just the current selected address.
+   */
+  public selectNone(): void {
+    for (var mail of this.inboxMessages) {
+      mail.selected = false;
+    }
+  }
+
+  /**
+   * Selects the `read` messages of the current selected address
+  */
+  public selectRead(): void {
+    this.selectNone();
+    this.currentAccountInboxMessages.filter(mail => mail.read).forEach(mail => mail.selected = true);
+  }
+
+  /**
+   * Selects the `unread` messages of the current selected address
+  */
+  public selectUnread(): void {
+    this.selectNone();
+    this.currentAccountInboxMessages.filter(mail => !mail.read).forEach(mail => mail.selected = true);
+  }
+
+  /**
+   * Marks the currently selected messages as `read`.
+   */
+  public markSelectedAsRead(): void {    
+    this.currentAccountInboxMessages.filter(message => message.selected).forEach(msg => {
+      var rcpt = this.parseAddressFromMailchain(msg["headers"]["to"])
+      // @TODO Add error handling
+      this.readService.markRead(msg["headers"]["message-id"]).subscribe(res => {
+        msg.read = true;
+        this.emitInboxCount([rcpt]);
+      })
+    });
+  }
+
+  /**
+   * Marks the currently selected messages as `unread`.
+   */
+  markSelectedAsUnread(): void {
+    this.inboxMessages.filter(message => message.selected).forEach(msg => {
+      var rcpt = this.parseAddressFromMailchain(msg["headers"]["to"])
+      // @TODO Add error handling
+      this.readService.markUnread(msg["headers"]["message-id"]).subscribe(res => {
+        msg.read = false;
+        this.emitInboxCount([rcpt]);
+      })
+    });
+  }
+
+
+  async ngOnInit(): Promise<void> {
+    this.getCurrentAccountInboxMessages()
+  }
+
+  /**
+   * Get the inbox messages, filtered by 'currently selected account' > 'searchtext'.
+   * Dedupe message array - workaround for dupe messages @TODO: waiting on dupe bugfix in mailchain
+   */
+  getCurrentAccountInboxMessages(){
+
+    this.currentAccountInboxMessages = this.inboxMessages
+
+  }
+
+  async ngOnChanges(event): Promise<void> {
+    // Handle resetting the selected mails
+    if ( 'currentAccount' in event) {
+      this.selectNone()
+    }
+    this.getCurrentAccountInboxMessages()
+  }
+
+}
--- src/app/inbox/inbox.component.ts
+++ src/app/inbox/inbox.component.ts
@@ -1,15 +1,216 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../services/helpers/local-storage/local-storage.service';
+import { PublicKeyService } from '../services/mailchain/public-key/public-key.service';
+import { MailchainService } from '../services/mailchain/mailchain.service';
+import { MessagesService } from '../services/mailchain/messages/messages.service';
+import { InboundMail } from '../models/inbound-mail';
 
 @Component({
   selector: 'app-inbox',
   templateUrl: './inbox.component.html',
-  styleUrls: ['./inbox.component.sass']
+  styleUrls: ['./inbox.component.scss'],
+  // providers: [ SearchPipe, AddressPipe ]
 })
 export class InboxComponent implements OnInit {
+  public fromAddresses: any = {};
+  public fromAddressesKeys: Array<any> = [];
+  public networks: Array<any> = [];
+  public inboxMessages: Array<any> = [];
+  public messagesLoaded: boolean = false;
+  public fetchMessagesDisabled: boolean = false;
+  public fetchMessagesText: String = "Check Messages";
+  
+  public inboxPartial: string = 'messages'
 
-  constructor() { }
+  public currentAccount: string;
+  public currentNetwork: string;
+  public currentMail: any;
+  public currentMessage: any;
+  
+  public accountIdenticons: any = {};
 
-  ngOnInit() {
+
+  constructor(
+    private localStorageService: LocalStorageService,
+    private publicKeyService: PublicKeyService,
+    private mailchainService: MailchainService,
+    private messagesService: MessagesService,
+  ) {}
+
+
+  // TODO: Can this be merged with below?
+  handleReply(message?): void {
+    this.currentMessage = void 0;
+    if (message != undefined) {
+      this.currentMessage = message;
+    }
+    this.changeView('compose');
+  }
+
+  handleCompose(message?): void {
+    this.currentMessage = void 0;
+    if (message != undefined) {
+      this.currentMessage = message;
+    }
+    this.changeView('compose');
+  }
+
+  /**
+   * [address: string,count: number]
+   */
+  onInboxCounter(array) {
+    var address: string = array[0] 
+    var count: number = array[1]
+    this.fromAddresses[address.toLowerCase()]["messageCount"]["inbox"] = count
+  }
+
+  onOpenMessage(mail: any): void {
+    this.currentMail = mail;
+    this.changeView('message');
+  }
+
+  changeView(inboxPartial: string): void {
+    this.inboxPartial = inboxPartial
+  }
+
+  /**
+   * Changes active folder in list
+   */
+  changeActiveItem(elementId): void {
+    document.querySelectorAll('.nav-item').forEach(element => {
+      element.classList.remove("active")
+    })
+    document.getElementById(elementId).classList.add("active");
+  }
+
+  /**
+   * Changes the local current Account and fetches mails for that account.
+   */
+  changeAccount(address){    
+    if (this.inboxPartial != 'messages') {
+      this.changeView('messages');
+    }
+    this.currentAccount = address
+    this.localStorageService.setCurrentAccount(this.currentAccount)
+  }
+
+  /**
+   * Changes the network and fetches mails for that network.
+   */
+  changeNetwork(){    
+    this.changeView('messages')
+    this.localStorageService.setCurrentNetwork(this.currentNetwork)
+    this.getMails()
+  }
+
+  public addressIsActive(address){
+    return address == this.currentAccount
+  }
+
+
+  async setFromAddressList(){
+    this.fromAddressesKeys = await this.publicKeyService.getPublicSenderAddresses();
+    
+    this.fromAddressesKeys.forEach(address => {
+      this.fromAddresses[address] = {
+        label: address,
+        value: address,
+        messageCount: {
+          inbox: 0,
+        }
+      }
+    });    
+  }
+
+  /**
+   * Set the list of networks in the dropdown
+   */
+  setNetworkList(){
+    var networks = this.mailchainService.getPublicNetworks();
+    networks.forEach(network => {
+      this.networks.push({
+        label: network,
+        value: network,
+      })
+    });
+  }
+
+  getIdenticon(address){
+    this.mailchainService.generateIdenticon(address)
+  }
+
+  /**
+   * Fetch account identicons for all fromAddresses and set the address value in accountIdenticons.
+   */
+  setAccountIdenticons() {
+    this.fromAddressesKeys.forEach(address => {
+      this.accountIdenticons[address] = this.mailchainService.generateIdenticon(address)
+    });    
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.currentAccount = await this.localStorageService.getCurrentAccount()
+    this.currentNetwork = this.localStorageService.getCurrentNetwork()
+    
+    await this.setFromAddressList()
+    this.getMails()
+    this.setNetworkList()
+    this.setAccountIdenticons()
+  }
+
+
+  /**
+   * Fetch mails from the server
+   */
+  public getMails(quiet?: boolean){
+    if (!quiet) {
+      this.inboxMessages = []
+      this.messagesLoaded = false;
+    }
+    this.fetchMessagesText = "Loading...";
+    this.fetchMessagesDisabled = true;
+    
+    let fetchCount = this.fromAddressesKeys.length // we count this down for each req
+    
+    this.fromAddressesKeys.forEach(address => {
+      var self = this
+      this.messagesService.getMessages(address, this.currentNetwork).subscribe(function(res){
+        // var counter = res["messages"].length
+        //!!!!!!!
+        let unreadMsgs = res["messages"].filter(msg => msg.status === "ok" && !msg.read);
+        let uniqUnreadMsgs = self.mailchainService.dedupeMessagesByIds(unreadMsgs)
+
+        self.onInboxCounter([address, uniqUnreadMsgs.length])
+        res["messages"].forEach(decryptedData => {
+          // only parse valid messages
+          if (decryptedData.status == "ok") {
+            self.addMailToInboxMessages(decryptedData);
+          }
+        });
+        --fetchCount
+        
+        if (fetchCount == 0 ) { // all get reqs should be complete
+          self.messagesLoaded = true;
+          self.fetchMessagesText = "Check Messages";
+          self.fetchMessagesDisabled = false;  
+        }
+      })
+      
+    });
+    
+  };
+
+    /**
+   * 
+   * @param decryptedMsg 
+   */
+  addMailToInboxMessages(decryptedMsg){  
+    decryptedMsg.senderIdenticon = this.mailchainService.generateIdenticon(decryptedMsg.headers.from)
+    var msg = {
+      ...new InboundMail,
+      ...decryptedMsg
+    }
+    this.inboxMessages.push(msg)
   }
 
 }
