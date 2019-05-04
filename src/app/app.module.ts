db9632df61201646e54a95a870c9082c97eb7671
Tim Boeckmann <tim@bespecific.co.uk>
2019-05-04 13:08:32 +0100

    build version for running locally

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpHelpersService } from './services/helpers/http-helpers/http-helpers.service';
 import { SearchPipe } from './pipes/search-pipe/search-pipe.pipe';
 import { AddressPipe } from './pipes/address-pipe/address-pipe.pipe';
+import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,12 @@ import { AddressPipe } from './pipes/address-pipe/address-pipe.pipe';
     BsDropdownModule.forRoot(),
     HttpClientModule
   ],
-  providers: [HttpHelpersService],
+  providers: [
+    HttpHelpersService,
+    { provide: APP_BASE_HREF, useValue: '/' },
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
