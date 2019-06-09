651098279973e6aa5f0481ccc622d6ffc906b8f4
Tim Boeckmann <tim@bespecific.co.uk>
2019-06-09 19:32:18 +0100

    Add versioning from package.json (to be semantic versioning)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { version } from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mailchain-web';
+  public version: string = version;
+  public title = 'Mailchain Inbox';
 }
