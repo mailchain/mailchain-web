5cfc8868df59cc0728a86c7b9df9b884f071a94e
Tim Boeckmann <tim@bespecific.co.uk>
2019-06-16 17:25:57 +0100

    add coveralls config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -11,6 +11,7 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-coveralls')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -20,7 +21,11 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml','coverage-istanbul'],
+    coverageReporter: {
+      type: 'lcov', // lcov or lcovonly are required for generating lcov.info files
+      dir: 'coverage/'
+    },
+    reporters: ['progress', 'kjhtml','coverage-istanbul','coveralls'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
