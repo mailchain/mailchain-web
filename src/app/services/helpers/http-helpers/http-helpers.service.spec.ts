1c3a621f8f96af112f4868a830b2c88038808bc1
Tim Boeckmann <tim@bespecific.co.uk>
2019-07-05 18:49:48 +0100

    add tests and document method

--- src/app/services/helpers/http-helpers/http-helpers.service.spec.ts
+++ src/app/services/helpers/http-helpers/http-helpers.service.spec.ts
@@ -1,5 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpHelpersService } from './http-helpers.service';
+import { HttpHeaders, HttpParams } from '@angular/common/http';
+
+let queryParams = [['myKey', 'myValue'], ['anotherKey', 'anotherValue']]
 
 describe('HttpHelpersService', () => {
   let httpHelpersService: HttpHelpersService;
@@ -16,4 +19,41 @@ describe('HttpHelpersService', () => {
   it('should be created', () => {
     expect(httpHelpersService).toBeTruthy();
   });
+
+  describe('getHttpOptions', () => {
+    it('should have a header: Content-Type equal to application/json', ()=>{
+      expect(httpHelpersService.getHttpOptions().headers.get('Content-Type')).toEqual('application/json')
+    })
+
+    it('should get the httpOptions without query params', ()=>{
+      expect(httpHelpersService.getHttpOptions().params.keys()).toEqual([])
+    });
+
+
+    it('should get the httpOptions with query params', ()=>{
+      expect(httpHelpersService.getHttpOptions(queryParams).params.keys()).toEqual(['myKey','anotherKey'])
+      expect(httpHelpersService.getHttpOptions(queryParams).params.toString()).toEqual('myKey=myValue&anotherKey=anotherValue')
+      expect(httpHelpersService.getHttpOptions(queryParams).params.get('myKey')).toEqual('myValue')
+    })
+  });
+
+  describe('setParams', () => {
+    it('should return httpOptions from setParams without query params', ()=>{
+      let httpOptions = {};
+      let expectedObj = new HttpParams
+      
+      expect(httpHelpersService.setParams(httpOptions).params).toEqual(expectedObj)
+    })
+    it('should return httpOptions from setParams with query params', ()=>{
+      let httpOptions = {};
+      
+      expect(httpHelpersService.setParams(httpOptions, queryParams).params.keys()).toEqual(['myKey','anotherKey'])
+
+      expect(httpHelpersService.setParams(httpOptions, queryParams).params.toString()).toEqual('myKey=myValue&anotherKey=anotherValue')
+
+      expect(httpHelpersService.setParams(httpOptions, queryParams).params.get('myKey')).toEqual('myValue')
+
+    })
+  });
+
 });
