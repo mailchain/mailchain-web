73bdcc7288aa6727ba8857b22efacb055d5b1e03
Tim Boeckmann <trb@bespecific.co.uk>
2019-11-17 14:28:49 +0000

    Addresses/protocol fix (#111)
    * fix tests
    
    * add protocol and network to addresses endpoint
    
    * fix tests
    
    * add protocol and network to addresses endpoint

--- src/app/services/mailchain/addresses/addresses.service.ts
+++ src/app/services/mailchain/addresses/addresses.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LocalStorageServerService } from '../../helpers/local-storage-server/local-storage-server.service';
+import { LocalStorageProtocolService } from '../../helpers/local-storage-protocol/local-storage-protocol.service';
+import { HttpHelpersService } from '../../helpers/http-helpers/http-helpers.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +10,14 @@ import { LocalStorageServerService } from '../../helpers/local-storage-server/lo
 export class AddressesService {
 
   private url: string
+  private protocol: string
+  private network: string
 
   constructor(
     private http: HttpClient,
+    private httpHelpersService: HttpHelpersService,
     private localStorageServerService: LocalStorageServerService,
+    private localStorageProtocolService: LocalStorageProtocolService,
     ) {
       this.initUrl()
     }
@@ -20,19 +26,27 @@ export class AddressesService {
      * Initialize URL from local storage
      */
     initUrl(){
-      this.url = `${this.localStorageServerService.getCurrentServerDetails()}/api`
+      this.url = `${this.localStorageServerService.getCurrentServerDetails()}/api`,
+      this.protocol = this.localStorageProtocolService.getCurrentProtocol()
+      this.network = this.localStorageServerService.getCurrentNetwork()
     }
 
   /**
    * Get and return the public addresses from my wallet to send from
    */
   async getAddresses() {
+    var httpOptions = this.httpHelpersService.getHttpOptions([
+      ['protocol',this.protocol],
+      ['network',this.network]
+    ])
+
     var addresses = []
     let res = await this.http.get(
-      this.url + `/addresses`
+      this.url + `/addresses`,
+      httpOptions
       // TODO handle failure
     ).toPromise();      
-    res["addresses"].forEach(address => {
+    res["body"]["addresses"].forEach(address => {
       addresses.push(this.handleAddressFormatting(address,'ethereum'))
     });
     return addresses
@@ -51,9 +65,14 @@ export class AddressesService {
    * Returns the addresses response with status codes
    */
   public getAddressesResponse(){
+    var httpOptions = this.httpHelpersService.getHttpOptions([
+      ['protocol',this.protocol],
+      ['network',this.network]
+    ])
+
     return this.http.get(
       this.url + `/addresses`,
-      {observe: 'response'}
+      httpOptions
     )
   }
 
