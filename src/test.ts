2dd159a43d3b70c253cac1ec3eef93ca60985a58
Tim B <tim@bespecific.co.uk>
2020-10-18 23:01:50 +0100

    clear sessionStorage before each test

--- src/test.ts
+++ src/test.ts
@@ -18,3 +18,7 @@ getTestBed().initTestEnvironment(
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
+
+beforeEach(function() {
+  sessionStorage.clear();
+});
