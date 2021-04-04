311a397e00fe763ec67ddadb93d36a14b52df6e5
Shivani Aggarwal <shivani.aggarwal2422@gmail.com>
2021-04-03 20:18:24 -0400

    Balance TDD

--- src/app/inbox/inbox-compose/inbox-compose.component.spec.ts
+++ src/app/inbox/inbox-compose/inbox-compose.component.spec.ts
@@ -47,6 +47,8 @@ describe('InboxComposeComponent', () => {
 
   const currentAccount = '0x92d8f10248c6a3953cc3692a894655ad05d61efb';
   const currentAccount2 = '0x0123456789012345678901234567890123456789';
+  const balance = '1.018 Eth';
+
   const ensName = 'mailchain.eth';
   const addresses = [currentAccount, currentAccount2];
 
@@ -133,6 +135,18 @@ describe('InboxComposeComponent', () => {
           await component.ngOnInit();
           expect(component.model.body).toBe('')
         })
+
+
+        describe('handling the balance', () => {
+          it('should initialize a balance in the "balance" field using an available balance', async () => {
+            component.balance = balance
+            await component.ngOnInit();
+            fixture.detectChanges()
+            expect(component.balance).toBe(balance)
+          })
+        });
+
+
 
         describe('handling the envelope', () => {
           it('should initialize an envelope in the "envelope" field using an available envelop', async () => {
@@ -603,3 +617,5 @@ describe('InboxComposeComponent', () => {
   });
 
 });
+
+
